d1559e0b0f3d49acc8141be4c9f6c11bb3106f4e
mikhail <mikhail.kuptsov.03@mail.ru>
2022-12-25 14:11:05 +0300

    Добавьте файлы проекта.

---
+++ Exseption/Exseption.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "date.h" 
+using namespace std;
+/*Функция вычисляет разность между двумя датами в днях. Даты представлены структурой с тремя полями: год, месяц, день.
+*/
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    using namespace date;
+    int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    try
+    {
+        sys_days birthday;
+        sys_days today = floor<days>(std::chrono::system_clock::now());
+        cout << (today - birthday).count() << endl;
+    }
+    catch (...) {
+        cout << "Данные введены не коректно";
+    }
+}
